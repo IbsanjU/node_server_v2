e707076ac5cc4fe244527a2ce89b0c59728b07ed
IbsanjU <bharath2668@gmail.com>
2021-08-05 20:18:44 +0530

    get user endpoint

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -2,5 +2,16 @@ const db = require('../models')
 const User = db.rest.models.user
 
 exports.getUser = async (req, res) => {
-  res.send({ message: 'Api Works...!' })
+  const { id } = req.params
+
+  const user = await User.findOne({
+    where: {
+      id
+    }
+  })
+
+  if (!user) {
+    return res.status(400).send({ message: `No user found with the id ${id}` })
+  }
+  return res.send(user)
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,11 @@
-const Router = require('express')
+const { Router } = require('express')
 const user = require('../controllers/user-controller')
 
 const router = Router()
 
 // Routes go here
 router.get('/:id', user.getUser)
+
+router.post('/createUser', user.createUser)
 
 module.exports = router
